d8a5856ffa094a700aaf373f89d8c11f67af5755
佐藤麻衣 <satomai@satoumainombp.lan>
2022-10-03 19:22:22 +0900

    一旦コミット

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :tasks
-    get 'tasks/:id/edit', to: 'tasks#edit'
+  get 'tasks/:id/edit', to: 'tasks#edit'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_03_025256) do
-  create_table "tasks", force: :cascade do |t|
-    t.string "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+ActiveRecord::Schema[7.0].define(version: 20_221_003_025_256) do
+  create_table 'tasks', force: :cascade do |t|
+    t.string 'title'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
